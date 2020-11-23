39e139f310a4b463642b7b8d8bfa574d9fb72220
John Kang <kbs8707@hotmail.com>
2020-11-23 16:43:52 -0500

    finished setting up view

--- routes/database.js
+++ routes/database.js
@@ -28,8 +28,9 @@ function getAccount(data, callback) {
 
 }
 
-//View 1
-function getBookInTransaction(callback) {
+//View 1: Computes a join of at least three tables
+//Displaying a book product, that is involved in a transaction
+function view1(callback) {
   let sql = "SELECT book.title, product.BookID, transaction.transactionID FROM book INNER JOIN product ON book.bookID=product.bookID INNER JOIN transaction ON product.productID=transaction.transactionID";
   connection.query(sql, function (err, res) {
     if (err) throw err;
@@ -38,8 +39,9 @@ function getBookInTransaction(callback) {
   });
 }
 
-//View 2
-function getCostHigher(data, callback) {
+//View 2: Uses nested queries with the ANY or ALL operator and uses a GROUP BY clause
+//Count the number of books that any author wrote that cost more than $500.
+function view2(data, callback) {
   if (!data) data = 500;
   let sql = "SELECT count(*) FROM book WHERE bookID = ANY(SELECT bookID FROM product WHERE price > " + data;
   connection.query(sql, function (err, res) {
@@ -49,8 +51,9 @@ function getCostHigher(data, callback) {
   });
 }
 
-//View 3
-function aboveScore(data, callback) {
+//View 3: A correlated nested query
+//To find and display each time a seller has a review score of 8 or above.
+function view3(data, callback) {
   if (!data) data = 8;
   let sql = `SELECT sellerinfo.sellerName, review.description FROM review, sellerinfo WHERE review.score =(SELECT review.score FROM review WHERE review.score > ${data})`;
   connection.query(sql, function (err, res) {
@@ -60,8 +63,9 @@ function aboveScore(data, callback) {
   });
 }
 
-//View 4
-function sellerEmail(callback) {
+//View 4: Uses a FULL JOIN
+//Select all seller email addresses
+function view4(callback) {
   let sql = "SELECT a.email FROM account a FULL JOIN sellerinfo s ON a.accountID = s.accountID";
   connection.query(sql, function (err, res) {
     if (err) throw err;
@@ -70,8 +74,9 @@ function sellerEmail(callback) {
   });
 }
 
-//View 5
-function sellerHasReview(callback) {
+//View 5: Uses nested queries with any of the set operations UNION, EXCEPT, or INTERSECT
+//Find all the sellers, except for ones with a review
+function view5(callback) {
   let sql = "SELECT sellerName FROM sellerinfo EXCEPT (SELECT sellerName FROM review)";
   connection.query(sql, function (err, res) {
     if (err) throw err;
@@ -80,8 +85,8 @@ function sellerHasReview(callback) {
   });
 }
 
-//View 6
-function uniqueBookCount(callback) {
+//View 6: Use count to find the number of unique books being sold
+function view6(callback) {
   let sql = "SELECT count(*) FROM (SELECT DISTINCT bookID FROM product)";
   connection.query(sql, function (err, res) {
     if (err) throw err;
@@ -90,8 +95,8 @@ function uniqueBookCount(callback) {
   });
 }
 
-//View 7
-function cheapBook(data, callback) {
+//View 7: Use ANY to find books that cost less than 20 and in new condition
+function view7(data, callback) {
   if (!data) data = 20;
   let sql = `SELECT title, author FROM book WHERE bookID = ANY (SELECT bookID FROM product WHERE (price < ${data}) AND condition = "New")`;
   connection.query(sql, function (err, res) {
@@ -101,10 +106,10 @@ function cheapBook(data, callback) {
   });
 }
 
-//View 8
-function authorMax(data, callback) {
+//View 8: Use Max to find the highest costing product from a specific Author
+function view8(data, callback) {
   if (!data) data = "J. K. Rowling";
-  let sql = `SELECT MAX(product.price) FROM product, book WHERE book.bookID = product.bookID AND book.author = ${data}`;
+  let sql = `SELECT MAX(product.price) FROM product, book WHERE book.bookID = product.bookID AND book.author = '${data}'`;
   connection.query(sql, function (err, res) {
     if (err) throw err;
     console.log(res); 
@@ -112,13 +117,18 @@ function authorMax(data, callback) {
   });
 }
 
-//View 9
-// function orderedAuthor(callback) {
-//   let sql = "SELECT COUNT(*) FROM book ORDER BY autho"
-// }
+//View 9: Use ORDER BY to retrieve a list of authors that are in the store
+function view9(callback) {
+  let sql = "SELECT * FROM book ORDER BY author";
+  connection.query(sql, function (err, res) {
+    if (err) throw err;
+    console.log(res); 
+    return callback(res);
+  });
+}
 
-//View 10
-function notSale(callback) {
+//View 10: Use NOT IN to find books that are not on the shelf
+function view10(callback) {
   let sql = `SELECT * FROM book WHERE bookID NOT IN (SELECT bookID FROM product)`;
   connection.query(sql, function (err, res) {
     if (err) throw err;
@@ -127,15 +137,26 @@ function notSale(callback) {
   });
 }
 
+//book API
+function bookAPI(data, callback) {
+  let sql = `SELECT * FROM book WHERE bookID = ${data}`;
+  connection.query(sql, function (err, res) {
+    if (err) throw err;
+    console.log(res); 
+    return callback(res);
+  });
+}
 module.exports = {
   getAccount,
-  getBookInTransaction,
-  getCostHigher,
-  aboveScore,
-  sellerEmail,
-  sellerHasReview,
-  uniqueBookCount,
-  cheapBook,
-  authorMax,
-  notSale
+  view1,
+  view2,
+  view3,
+  view4,
+  view5,
+  view6,
+  view7,
+  view8,  
+  view9,
+  view10,
+  bookAPI
 };
--- routes/index.js
+++ routes/index.js
@@ -9,8 +9,94 @@ router.get('/', function(req, res, next) {
     console.log(out);
     res.render('index', { title: result[0].accountID });
   });
+});
 
+router.get('/view1', function(req, res, next) {
 
+  db.view1(function(result){
+    console.log(out);
+    res.render('index', { title: result[0].accountID });
+  });
+});
+
+router.get('/view2', function(req, res, next) {
+
+  db.view2('', function(result){
+    console.log(out);
+    res.render('index', { title: result[0].accountID });
+  });
+});
+
+router.get('/view3', function(req, res, next) {
+
+  db.view3('', function(result){
+    console.log(out);
+    res.render('index', { title: result[0].accountID });
+  });
+});
+
+router.get('/view4', function(req, res, next) {
+
+  db.view4(function(result){
+    console.log(out);
+    res.render('index', { title: result[0].accountID });
+  });
+});
+
+router.get('/view5', function(req, res, next) {
+
+  db.view5(function(result){
+    console.log(out);
+    res.render('index', { title: result[0].accountID });
+  });
+});
+
+router.get('/view6', function(req, res, next) {
+
+  db.view6(function(result){
+    console.log(out);
+    res.render('index', { title: result[0].accountID });
+  });
+});
+
+router.get('/view7', function(req, res, next) {
+
+  db.view7('', function(result){
+    console.log(out);
+    res.render('index', { title: result[0].accountID });
+  });
+});
+
+router.get('/view8', function(req, res, next) {
+
+  db.view8('', function(result){
+    console.log(out);
+    res.render('index', { title: result[0].accountID });
+  });
+});
+
+router.get('/view9', function(req, res, next) {
+
+  db.view9(function(result){
+    console.log(out);
+    res.render('index', { title: result[0].accountID });
+  });
+});
+
+router.get('/view10', function(req, res, next) {
+
+  db.view10('', function(result){
+    console.log(out);
+    res.render('index', { title: result[0].accountID });
+  });
+});
+
+router.get('/bookapi', function(req, res, next) {
+  let data = req.query.id;
+  db.bookAPI(data, function(result){
+    console.log(out);
+    res.render('index', { title: result[0].accountID });
+  });
 });
 
 module.exports = router;
